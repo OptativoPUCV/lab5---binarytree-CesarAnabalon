299862c224eb0ba494a9c295b5aa0acdc4df6d74
Replit user <>
2023-04-22 14:09:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,17 +48,37 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2))
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) 
 {
-  TreeNode* actual = NULL;
-  
-  while(actual != NULL)
-  {
-    if(is_equal(tree, key, actual->pair->key ))
-     {
-       return ;
-     }
-  }
-  
-  
+  TreeNode* current = tree->root;
+    TreeNode* parent = NULL;
+    while (current != NULL) {
+        parent = current;
+        if (is_equal(tree, key, current->pair->key)) {
+            // la clave ya existe, así que no insertamos nada
+            return;
+        } else if (tree->lower_than(key, current->pair->key)) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    // creamos un nuevo nodo con el par clave-valor
+    TreeNode* new_node = createTreeNode(key, value);
+
+    // lo enlazamos en la ubicación encontrada
+    new_node->parent = parent;
+    if (parent == NULL) {
+        // el árbol está vacío, así que el nuevo nodo es la raíz
+        tree->root = new_node;
+    } else if (tree->lower_than(key, parent->pair->key)) {
+        parent->left = new_node;
+    } else {
+        parent->right = new_node;
+    }
+
+    // hacemos que el current apunte al nuevo nodo
+    tree->current = new_node;
+}
 }
 
 TreeNode * minimum(TreeNode * x){
